a10128e557bc36ac62b959590ffe7e66b0f0971a
truongtienn2001 <truongtienn2001@gmail.com>
2022-11-18 22:58:12 +0700

    commit

--- src/component/DetailSongs.js
+++ src/component/DetailSongs.js
@@ -5,9 +5,11 @@ export default function DetailSongs() {
   const {song} = useContext(Songs)
   return (
 
-    <div className='col-span-1 p-3'>
+    <div className='col-span-1 p-3 '>
+      <div className='fixed'>
       <h2 className='text-cyan-400 font-bold'>Now playing...</h2>
       <h2 className='text-stone-500 text-2xl'>Sing me to sleep</h2>
+      
       <div className='w-[240px] m-auto mt-5'>
           <img className='w-full rounded-3xl' src={song.links.images[0].url} alt='avatar'/>
       </div>
@@ -15,6 +17,7 @@ export default function DetailSongs() {
         <img className='w-[70px] rounded-full' src={song.links.images[1].url} alt='avatar'/>
         <span className='text-xl text-white'>{song.author}</span>
       </div>
+    </div>
     </div>
   )
 }
